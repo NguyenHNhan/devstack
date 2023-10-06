2244a8efabe673747a153d6c0cf08cd26491e175
NguyenHNhan <115022604+NguyenHNhan@users.noreply.github.com>
2023-10-06 09:32:01 +0700

    Update devstack.sh

--- devstack.sh
+++ devstack.sh
@@ -4,4 +4,9 @@ cd devstack/
 
 cp samples/local.conf .
 
+sed -i 's/ADMIN_PASSWORD=.*/ADMIN_PASSWORD=123/' local.conf
+sed -i 's/DATABASE_PASSWORD=.*/DATABASE_PASSWORD=123/' local.conf
+sed -i 's/RABBIT_PASSWORD=.*/RABBIT_PASSWORD=123/' local.conf
+sed -i 's/SERVICE_PASSWORD=.*/SERVICE_PASSWORD=$ADMIN_PASSWORD/' local.conf
+
 sudo nano local.conf
